e7a36a1f2004b25d7ce6eddaf570bdd023ba7a20
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-08-10 15:25:04 +0530

    cartscreen img , name, price, total created

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,37 +1,65 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Message from "./../components/Message";
+
 const CartScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <>
       <h1 className="mb-3">Shopping Cart</h1>
-      <div className="row">
-        <div className="col-md-8 ">
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <div className="row">
-                <div className="col-md-2 ">image</div>
-                <div className="col-md-4 ">name</div>
-                <div className="col-md-2 ">price</div>
-                <div className="col-md-2 ">qty</div>
-                <div className="col-md-2 ">delete</div>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="row">
-                <div className="col-md-2 ">image</div>
-                <div className="col-md-4 ">name</div>
-                <div className="col-md-2 ">price</div>
-                <div className="col-md-2 ">qty</div>
-                <div className="col-md-2 ">delete</div>
-              </div>
-            </li>
-          </ul>
+
+      {cartItems.length === 0 ? (
+        <Message>
+          Your cart is empty <Link to="/">Go Back</Link>
+        </Message>
+      ) : (
+        <div className="row">
+          <div className="col-md-8 ">
+            <ul className="list-group list-group-flush">
+              {cartItems.map((item) => (
+                <li className="list-group-item" key={item._id}>
+                  <div className="row">
+                    <div className="col-md-2 ">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="img-fluid rounded"
+                      />
+                    </div>
+                    <div className="col-md-4 ">
+                      <Link to={`/product/${item._id}`} className="text-dark">
+                        {item.name}
+                      </Link>
+                    </div>
+                    <div className="col-md-2 ">${item.price}</div>
+                    <div className="col-md-2 ">qty</div>
+                    <div className="col-md-2 ">delete</div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <ul className="list-group shadow">
+              <li className="list-group-item ">
+                <div className="row my-3">
+                  <div className="col-6">Total item quantity </div>
+                  <div className="col-6">
+                    {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                  </div>
+                </div>
+
+                <div className="row my-3">
+                  <div className="col-6">Total item price </div>
+                  <div className="col-6">${cart.itemsPrice}</div>
+                </div>
+              </li>
+              <li className="list-group-item">checkout</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-md-4">
-          <ul className="list-group shadow">
-            <li className="list-group-item">total</li>
-            <li className="list-group-item">checkout</li>
-          </ul>
-        </div>
-      </div>
+      )}
     </>
   );
 };
